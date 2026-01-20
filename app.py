a2f0d17ff9a46395a1b7e08a0d6a3c3ab8208fe2
Gajanan Patil <gajanan.patil@example.com>
2026-01-20 13:18:32 +0530

    updtaed

--- app.py
+++ app.py
@@ -1,4 +1,31 @@
-import streamlit as st
+# import streamlit as st
+# import pandas as pd
+# import seaborn as sns
+# data = pd.read_csv("mobile_sales.csv")
 
-st.write("dbsi")
+# data
 
+# x = st.slider('x')  # 👈 this is a widget
+# st.write(x, 'squared is', x * x)
+
+
+
+# st.text_input("Your name", key="name")
+
+# # You can access the value at any point with:
+# st.session_state.name
+
+
+# st.pyplot(sns.pairplot(data))
+
+# st.dataframe(data)
+
+# st.write(data.columns)
+
+# st.write(data.describe())
+
+# st.write(data.size)
+# st.write(data.shape)
+
+
+# st.sidebar.markdown("# Main page 🎈")
